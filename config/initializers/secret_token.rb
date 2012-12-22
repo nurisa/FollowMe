7ce2a384bf2d91e3ec8cb9952829c5daf2951e18
nurisa <dhimas.yudhie03@gmail.com>
2012-12-22 09:14:11 -0500

    initial of project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FollowMe::Application.config.secret_token = '12c3ab3d66479b446c4eb315f5a0ceeaedcbac53ef2cdebe2167a9f55d9e0bbf93528d6ad1d54c12d38c77213e8d9e9cd66c7af817ed7a598a8a752f486f8a52'
